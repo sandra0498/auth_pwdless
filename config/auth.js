f74589d060294729ec6d29ddcb8a7bed8e5487f2
Sandra Chavez <chavezsandra0498@gmail.com>
2022-10-14 18:09:35 -0700

    Created hooks for email and code

--- config/auth.js
+++ config/auth.js
@@ -3,7 +3,7 @@ import { WebAuth } from "auth0-js";
 export const webAuth = new WebAuth({
     clientID: 'wfKM8a6WO6mZARYEN9yB2fCFIgrtuh8K',
     domain: 'dev-8xxbv-mc.us.auth0.com',
-    redirectUri: 'https://ueofp.sse.codesandbox.io/coupon',
+    redirectUri: 'http://localhost:4040',
     responseType: "token"
 });
 
